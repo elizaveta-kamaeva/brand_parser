e1504c72b90f465dbf36e2a6b962f6d6a714c86d
Elizaveta Kamaeva <elizaveta.kamaeva@diginetica.com>
2019-01-28 18:24:52 +0300

    edited

--- parse_wine-shopper.py
+++ parse_wine-shopper.py
@@ -1,6 +1,4 @@
 import re
-import requests
-from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 
 
@@ -12,26 +10,27 @@ offers = root.findall("offers")[0]
 titles = set()
 pairs = []
 n = 0
+num_wrong = 0
 
 for child in offers.findall("offer"):
     for param in child:
         if param.tag == 'name':
             titles.add(param.text)
             n += 1
-    if n % 200 == 0:
+    if n % 1000 == 0:
         print(n, 'names done.')
 
 outfile = open('files\\wineshopper_parsed.csv', 'w', encoding='utf-8')
-print('Unequal strings:')
+
 for name in titles:
     try:
-        eng_str, trans_str = name.split('(')
+        eng_str, trans_str = name.strip().split('(')
     except ValueError:
         continue
 
     # remove size
-    eng_str = re.sub('\d+[.,]\d+ \w \Z', '', eng_str)
-    trans_str = re.sub('\d+[.,]\d+ \w\)\Z', '', trans_str)
+    eng_str = re.sub('\d+[.,]\d+\s?\w \Z', '', eng_str)
+    trans_str = re.sub('\d+[.,]\d+\s?\w\)\Z', '', trans_str)
 
     # find only letters, separated by ['`-]
     eng_list = re.findall('[A-Za-zÀ-ʯ]+'
@@ -40,10 +39,10 @@ for name in titles:
                             '(?:[\'’`-][А-Яа-яЁё]+)*', trans_str)
 
     if eng_list:
-        outfile.write('{};{}\n'.format(' '.join(eng_list), ' '.join(trans_list)))
-
         if len(eng_list) != len(trans_list):
-            print(name)
+            num_wrong += 1
+        outfile.write('{};{}\n'.format(' '.join(eng_list), ' '.join(trans_list)))
 outfile.close()
 
 print('Parsing done. {} lines have been processed.'.format(n))
+print('Number of unequal lines:', num_wrong)
